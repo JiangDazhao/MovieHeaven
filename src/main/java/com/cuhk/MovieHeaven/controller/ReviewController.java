5d993bca8ea14babbd1253b9b7c030dbdb68eed4
codeworm111 <1169872430@qq.com>
2023-03-09 21:52:12 +0800

    [finish] ReviewController, you can add review in one movie

--- src/main/java/com/cuhk/MovieHeaven/controller/ReviewController.java
+++ src/main/java/com/cuhk/MovieHeaven/controller/ReviewController.java
@@ -1,7 +1,30 @@
 package com.cuhk.MovieHeaven.controller;
 
+import com.cuhk.MovieHeaven.entity.Review;
+import com.cuhk.MovieHeaven.service.ReviewService;
+import com.cuhk.MovieHeaven.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
 
 @Controller
+@RequestMapping(path="/review")
 public class ReviewController {
+    @Autowired
+    HostHolder hostHolder;
+    @Autowired
+    ReviewService reviewService;
+
+    @RequestMapping(path = "/add/{movieId}",method = RequestMethod.POST)
+    public String addReview(@PathVariable("movieId") int movieId, Review review){ // review里面已经携带了name="stars"，name="reviewContent"，name="movieId"
+        review.setUserId(hostHolder.getUser().getUserId());
+        review.setReviewTime(new Date());
+        reviewService.addReview(review);
+        
+        return "redirect:/moviepost/detail/"+movieId; // 加了review以后继续重定向到该movie的详情页面上
+    }
 }
